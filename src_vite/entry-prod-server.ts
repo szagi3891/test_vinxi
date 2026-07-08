954b73b5b0c9e3699686532c9b8b1126c65635d2
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2026-07-08 23:30:25 +0200

    szkic

--- src_vite/entry-prod-server.ts
+++ src_vite/entry-prod-server.ts
@@ -22,3 +22,28 @@ const handleRequest = createRequestHandler(serveStatic);
 
 console.log(`Production server: http://localhost:${port}`);
 Deno.serve({ port }, handleRequest);
+
+
+/*
+
+  1. przykładowa struktura
+
+  src
+    app
+      entry-client/ -- tutaj jest aplikacja reactowa
+      entru-api/ -- tutaj jest api serwera
+
+      entry-dev-server.ts - dev server
+      entry-prod-server.ts - prod server
+
+      vite.config.ts - konfiguracja
+
+
+
+    2. dodać testową aplikację desktop
+
+    src/desktop ...
+     zobaczyć czy się ten wzór dobrze komponuje
+
+    3. dodać przykładowe endpointy z orpc, jak się to komponuje
+*/
