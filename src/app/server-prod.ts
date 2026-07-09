71287304346832f1d125950643d8a3cbe116f802
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2026-07-09 08:12:45 +0200

    debug

--- src/app/server-prod.ts
+++ src/app/server-prod.ts
@@ -6,6 +6,13 @@ const clientDist = new URL("../client/", import.meta.url);
 const clientIndex = new URL("../client/index.html", import.meta.url);
 const port = 3000;
 
+console.info('clientDist', clientDist.pathname);
+console.info('clientIndex', clientIndex.pathname);  
+
+const index = await Deno.readFile(clientIndex.pathname);
+const indexText = new TextDecoder().decode(index);
+console.info('indexText', indexText);
+
 async function serveStatic(request: Request): Promise<Response> {
   const response = await serveDir(request, {
     fsRoot: clientDist.pathname,
